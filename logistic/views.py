8678c6b101681ce9cb5f98041f01063082027792
Muxriddin <muxri1334@mail.ru>
2023-03-04 23:09:06 +0300

    Update file views.py

--- logistic/views.py
+++ logistic/views.py
@@ -32,4 +32,4 @@ class ProductPositionSet(ModelViewSet):
 
 class TestView(APIView):
     def get(self, request):
-        return Response({"Message": "test"})
+        return Response({"Message": "text"})
